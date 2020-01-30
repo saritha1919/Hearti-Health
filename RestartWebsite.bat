4574fce791179a0aecfba80271668f85115005ca
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-30 15:59:47 +0530

    Update and rename iisbat.bat to RestartWebsite.bat

--- RestartWebsite.bat
+++ RestartWebsite.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-SET ThisScriptsDirectory=C:\WindowService\batfile
+SET ThisScriptsDirectory="C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Angular"
 SET PowerShellScriptPath=%ThisScriptsDirectory%StopWebsite.ps1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
 pause
