d25a0e76c5fd6b47213592e87c6cc550c00b7509
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-30 16:32:03 +0530

    Update RestartWebsite.bat

--- RestartWebsite.bat
+++ RestartWebsite.bat
@@ -1,5 +1,3 @@
 @ECHO OFF
-SET ThisScriptsDirectory="C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Angular"
-SET PowerShellScriptPath=%ThisScriptsDirectory%StopWebsite.ps1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
-pause
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-AngularStopWebsite.ps1"' -Verb RunAs}";
+
