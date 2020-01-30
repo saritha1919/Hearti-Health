06eaf9aa721daadca2b64d18f77e4e351b15238d
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-30 18:58:15 +0530

    Update RestartWebsite.bat

--- RestartWebsite.bat
+++ RestartWebsite.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-powershell.exe -File "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Angular\StopWebsite.ps1" -ExecutionPolicy Bypass
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Angular\StopWebsite.ps1" -ExecutionPolicy Bypass
