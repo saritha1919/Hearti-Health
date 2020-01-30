c4f4b26162c0b5b61b8669e34c34b334dd52cef3
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-30 19:04:32 +0530

    Update StopWebsite.ps1

--- StopWebsite.ps1
+++ StopWebsite.ps1
@@ -1,9 +1,10 @@
-$Website = "Test-Angular"
-$WebsiteStatus = (Get-WebsiteState -Name $Website).value
-if(!($WebsiteStatus -eq 'Stopped')){
-Stop-Website -Name $Website
-}
-$WebAppPoolStatus = (Get-WebAppPoolState -Name $Website).value
-if(!($WebAppPoolStatus -eq 'Stopped')){
-Stop-WebAppPool -Name $Website
-}
+#$Website = "Test-Angular"
+#$WebsiteStatus = (Get-WebsiteState -Name $Website).value
+#if(!($WebsiteStatus -eq 'Stopped')){
+#Stop-Website -Name $Website
+#}
+#$WebAppPoolStatus = (Get-WebAppPoolState -Name $Website).value
+#if(!($WebAppPoolStatus -eq 'Stopped')){
+#Stop-WebAppPool -Name $Website
+#}
+Stop-WebSite -Name "Test-Angular"
