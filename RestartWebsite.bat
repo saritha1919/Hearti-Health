10b45d206e4f13d5fdd29e87072d93e375cf0fb2
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-30 18:12:47 +0530

    Delete RestartWebsite.bat

--- RestartWebsite.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File ""C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Angular\StopWebsite.ps1""'}"
